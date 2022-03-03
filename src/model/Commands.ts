6b1e111a12619f726f4c951355ccf30a3d4a0203
joelvitorniino <joelprofessor13@gmail.com>
2022-03-03 17:57:10 -0300

    Removing imports of commands

--- src/model/Commands.ts
+++ src/model/Commands.ts
@@ -1,9 +1,7 @@
-import { WAMessage } from "@adiwajshing/baileys";
 import { CommandService } from "./services/CommandService";
-import { JishoCommandService } from "./services/JishoCommandService";
 
 export class Commands {
-    private _service: CommandService;
+    private _service: CommandService;s
 
     constructor(_service: CommandService) {
         this._service = _service;
