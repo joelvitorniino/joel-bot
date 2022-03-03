b3babad29876634d7bb04a04fa0b1cc6fced7c6e
joelvitorniino <joelprofessor13@gmail.com>
2022-03-03 18:00:30 -0300

    Removing letter

--- src/model/Commands.ts
+++ src/model/Commands.ts
@@ -1,7 +1,7 @@
 import { CommandService } from "./services/CommandService";
 
 export class Commands {
-    private _service: CommandService;s
+    private _service: CommandService;
 
     constructor(_service: CommandService) {
         this._service = _service;
