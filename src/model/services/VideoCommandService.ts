f13e9cef83c38234485320a533f27652901f42df
joelvitorniino <joelprofessor13@gmail.com>
2022-03-19 02:26:49 -0300

    Correcting Command Video

--- src/model/services/VideoCommandService.ts
+++ src/model/services/VideoCommandService.ts
@@ -1,26 +1,29 @@
-import { WAMessage } from "@adiwajshing/baileys";
+import { WAMessage, WASocket } from "@adiwajshing/baileys";
 import axios from "axios";
 import { IVideo } from "../../interfaces/IVideo";
 import { commandSlice } from "../../utils/commandSlice";
 import { CommandService } from "./CommandService";
 
 export class VideoCommandService implements CommandService {
-  constructor(public sock, public jid: String, public msg: WAMessage) {
-    this.sendCommand(sock, jid, msg);
+  private msg: WAMessage;
+  private sock: WASocket;
+
+  constructor(public jid: string) {
+    this.sendCommand(jid);
   }
 
-  async sendCommand(sock, jid: String, msg: WAMessage) {
-    const body = String(msg.message.conversation);
+  async sendCommand(jid: string) {
+    const body = String(this.msg.message.conversation);
     const bodySlice = commandSlice(body, 2);
 
-    if (msg.message.conversation === `!v ${bodySlice}`) {
+    if (this.msg.message.conversation === `!v ${bodySlice}`) {
       const bodyURI = encodeURI(bodySlice);
 
       const response = await axios.get<IVideo>(
         `https://api-get-info-youtube.herokuapp.com/api/v1/video?link=${bodyURI}`
       );
 
-      sock.sendMessage(jid, {
+      this.sock.sendMessage(jid, {
         video: {
           url: response.data.linkVideo,
         },
