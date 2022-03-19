561c58d63123ff416ce8b143b7c4627dea7f7390
joelvitorniino <joelprofessor13@gmail.com>
2022-03-19 02:11:55 -0300

    Correcting Command Menu

--- src/model/services/MenuCommandService.ts
+++ src/model/services/MenuCommandService.ts
@@ -1,18 +1,21 @@
-import { WAMessage } from "@adiwajshing/baileys";
+import { WAMessage, WASocket } from "@adiwajshing/baileys";
 import fs from 'fs';
 import { CommandService } from "./CommandService";
 
 export class MenuCommandService implements CommandService {
-    constructor(public sock, public jid: String, public msg: WAMessage) {
-        this.sendCommand(sock, jid, msg);
+    private msg: WAMessage;
+    private sock: WASocket;
+
+    constructor(public jid: string) {
+        this.sendCommand(jid);
     };
 
-    sendCommand(sock, jid: String, msg: WAMessage) {
-        if(msg.message.conversation.startsWith('!') && msg.message.conversation === '!menu') {
+    sendCommand(jid: string) {
+        if(this.msg.message.conversation.startsWith('!') && this.msg.message.conversation === '!menu') {
             fs.readFile(`/home/joel/Documents/joel-bot/src/md/menu.md`, 'utf-8', (err, data) => {
                 if(err) throw err;
 
-                sock.sendMessage(jid, { text: data });
+                this.sock.sendMessage(jid, { text: data });
             });
         };        
     };
