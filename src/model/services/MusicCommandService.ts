f155db18699e8e22276ef6541a65c40cdd08d223
joelvitorniino <joelprofessor13@gmail.com>
2022-03-19 02:17:02 -0300

    Correcting Command Music

--- src/model/services/MusicCommandService.ts
+++ src/model/services/MusicCommandService.ts
@@ -1,24 +1,27 @@
-import { WAMessage } from "@adiwajshing/baileys";
+import { WAMessage, WASocket } from "@adiwajshing/baileys";
 import axios from 'axios';
 import { IMusic } from "../../interfaces/IMusic";
 import { commandSlice } from "../../utils/commandSlice";
 import { CommandService } from "./CommandService";
 
 export class MusicCommandService implements CommandService {
-    constructor(public sock, public jid: String, public msg: WAMessage) {
-        this.sendCommand(sock, jid, msg);
+    private msg: WAMessage;
+    private sock: WASocket;
+
+    constructor(public jid: string) {
+        this.sendCommand(jid);
     };
 
-    async sendCommand(sock, jid: String, msg: WAMessage) {
-        const body = String(msg.message.conversation);
+    async sendCommand(jid: string) {
+        const body = String(this.msg.message.conversation);
         const sliceBody = commandSlice(body, 2);
 
-        if(msg.message.conversation === `!m ${sliceBody}`) {
+        if(this.msg.message.conversation === `!m ${sliceBody}`) {
             const bodyURI = encodeURI(sliceBody);
 
             const response = await axios.get<IMusic>(`https://api-get-info-youtube.herokuapp.com/api/v1/music?link=${bodyURI}`);
 
-            await sock.sendMessage(jid, {
+            await this.sock.sendMessage(jid, {
                 audio: {
                     url: response.data.linkMusic
                 },
