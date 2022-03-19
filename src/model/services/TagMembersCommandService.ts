75ef73262ea7adcd63c04ab4914c76403f592bb1
joelvitorniino <joelprofessor13@gmail.com>
2022-03-19 02:22:48 -0300

    Correcting Command TagMembers

--- src/model/services/TagMembersCommandService.ts
+++ src/model/services/TagMembersCommandService.ts
@@ -1,21 +1,24 @@
-import { WAMessage } from "@adiwajshing/baileys";
+import { WAMessage, WASocket } from "@adiwajshing/baileys";
 import { CommandService } from "./CommandService";
 
 export class TagMembersCommandService implements CommandService {
-    constructor(public sock, public jid: String, public msg: WAMessage) {
-        this.sendCommand(sock, jid, msg);
+    private msg: WAMessage;
+    private sock: WASocket;
+
+    constructor(public jid: string) {
+        this.sendCommand(jid);
     };
 
-    async sendCommand(sock, jid: String, msg: WAMessage) {
+    async sendCommand(jid: string) {
 
-        if(msg.message.conversation === '!tagmembers') {
-            const metadata = await sock.groupMetadata(jid, false);
+        if(this.msg.message.conversation === '!tagmembers') {
+            const metadata = await this.sock.groupMetadata(jid);
             const array = metadata.participants.map(all => all.id);
             let allMembers = "";
             
             array.forEach((participant, i) => allMembers += `@${array[i].replace('@s.whatsapp.net', '')}\n`);
     
-            await sock.sendMessage(jid, {
+            await this.sock.sendMessage(jid, {
                 text: allMembers,
                 mentions: array
             });    
