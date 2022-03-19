218a61f41545893c11e7c1bf9d693f3baa55595e
joelvitorniino <joelprofessor13@gmail.com>
2022-03-19 02:48:56 -0300

    Correcting Command Jisho with new Parameter

--- src/model/services/JishoCommandService.ts
+++ src/model/services/JishoCommandService.ts
@@ -5,21 +5,19 @@ import { commandSlice } from '../../utils/commandSlice';
 import { CommandService } from './CommandService';
 
 export class JishoCommandService implements CommandService {
-    private msg: WAMessage;
-    private sock: WASocket;
 
-    constructor(public jid: string) {
-        this.sendCommand(jid);
+    constructor(public sock: WASocket, public jid: string, public msg: WAMessage) {
+        this.sendCommand(sock, jid, msg);
     };
 
-    async sendCommand(jid: string) {
-        const bodySlice = commandSlice(this.msg.message.conversation, 6);
+    async sendCommand(sock: WASocket, jid: string, msg: WAMessage) {
+        const bodySlice = commandSlice(msg.message.conversation, 6);
         const bodyURI = encodeURI(bodySlice);
 
-        if(this.msg.message.conversation === `!jisho ${bodySlice}`) {
+        if(msg.message.conversation === `!jisho ${bodySlice}`) {
             const response = await axios.get<IJisho>(`https://jisho.org/api/v1/search/words?keyword=${bodyURI}`);
 
-            await this.sock.sendMessage(jid, {
+            await sock.sendMessage(jid, {
             text: `Slug: ${response.data.data[0].slug}\nReading: ${response.data.data[0].senses[0].english_definitions[0]}`
             });
         };
